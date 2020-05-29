c414da4ae947469f7d84d2d4bb954ce29e1ca448
Alex Torscho <contact@alextorscho.com>
2020-05-29 23:02:35 +0200

    Deleting old version

--- resources/views/foundation-nav.blade.php
+++
@@ -1,7 +0,0 @@
-<nav class="breadcrumbs">
-	@foreach ($crumbs as $crumb)
-		<a class="{{ $crumb->active(false, 'current') }} {{ $crumb->disabled('unavailable') }}" href="{{ $crumb->url }}">
-			{!! $crumb->title !!}
-		</a>
-	@endforeach
-</nav>
